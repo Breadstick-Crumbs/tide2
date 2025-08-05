40a1b1487616477e07510ab736ea353ec2c424e6
Anees <53440937+Breadstick-Crumbs@users.noreply.github.com>
2025-08-06 02:07:08 +0400

    Update DataQADao.java

--- DataQADao.java
+++ DataQADao.java
@@ -1,5 +1,6 @@
 package com.tridel.tems_data_service.dao;
 
+import com.tridel.tems_data_service.model.request.DeleteQCRequest;
 import com.tridel.tems_data_service.model.request.EditQCRequest;
 import com.tridel.tems_data_service.model.request.ReportRequest;
 import com.tridel.tems_data_service.model.request.SensorParamViewReq;
@@ -9,6 +10,9 @@ public interface DataQADao {
     String loadAllQCData(ReportRequest request);
 
     String editQCData(EditQCRequest req);
+
+    int deleteQCData(DeleteQCRequest req);
+
 
 
 }
