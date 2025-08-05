bb90eae06665e0b7bcc6bbb35283125bc7bb8a3d
Anees <53440937+Breadstick-Crumbs@users.noreply.github.com>
2025-08-06 02:06:01 +0400

    Update DataQAService.java

--- DataQAService.java
+++ DataQAService.java
@@ -1,12 +1,15 @@
 package com.tridel.tems_data_service.service;
 
 
+import com.tridel.tems_data_service.model.request.DeleteQCRequest;
 import com.tridel.tems_data_service.model.request.EditQCRequest;
 import com.tridel.tems_data_service.model.request.ReportRequest;
 
 public interface DataQAService {
     String loadAllQCData(ReportRequest req);
     String editQCData(EditQCRequest req);
+    String deleteQCData(DeleteQCRequest req);
+
 
 
 }
